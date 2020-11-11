12cbd71b0340ad64bfcda792788e69f122a2dc9a
Matheus Filipe dos Santos Reinert <62085690+shinobu-uwu@users.noreply.github.com>
2020-11-11 13:46:26 -0300

    Criar key para cancelar

--- inserir_comando_view.py
+++ inserir_comando_view.py
@@ -6,7 +6,7 @@ class InserirComandoView:
         self.__layout = [
             [sg.Text("Selecione um tipo de comando"), sg.Listbox([comando for comando in bot.comandos], size = (30, 3), key = "comandos")],
             [sg.Text("Digite o comando"), sg.Input(key = "comando")],
-            [sg.Button("Inserir", key = "inserir"), sg.Cancel()]
+            [sg.Button("Inserir", key = "inserir"), sg.Cancel("Cancelar", key = "cancelar")]
         ]
         self.__window = sg.Window("Inserir comando", self.__layout, element_justification = "center")
 
