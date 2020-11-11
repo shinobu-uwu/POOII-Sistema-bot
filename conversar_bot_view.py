6d212d2ed4c277484a259808a2a16c06f9f16dea
Matheus Filipe dos Santos Reinert <62085690+shinobu-uwu@users.noreply.github.com>
2020-11-11 14:19:55 -0300

    Create conversar_bot_view.py

---
+++ conversar_bot_view.py
@@ -0,0 +1,28 @@
+import PySimpleGUI as sg
+
+class MainView:
+    def __init__(self):
+        self.__theme = sg.theme("DarkAmber")
+        self.__layout = []
+        self.__window = sg.Window('Sistema Chatbot', self.__layout)
+
+    def mostra_view(self, bots):
+        self.__layout = [
+                            [sg.Text("Olá, esse é o sistema de chatbots da empresa CrazyBots")],
+                            [sg.Listbox(bots, key = "lista_bots", size = (30, 5))],
+                            [sg.Button("Conversar", key = "conversar"), sg.Button("Importar", key = "importar"),
+                             sg.Button("Exportar bot", key = "exportar"), sg.Button("Atualizar", key = "refresh"),
+                             sg.Button("Remover bot", key = "remover"), sg.Button("Criar bot", key = "criar"),],
+                            [sg.Button("Adicionar comando", key = "adicionar_comando")]
+                        ]
+        self.__window = sg.Window("Sistema Chatbot", self.__layout, element_justification = "center")
+        return self.__layout
+
+    def atualiza_elemento(self, elemento, valor):
+        self.__window.Element(elemento).Update(valor)
+
+    def le_eventos(self):
+        return self.__window.Read()
+
+    def fechar(self):
+        self.__window.close()
