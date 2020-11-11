70ff6940209b50b943c36d0ea24540ae95201776
Matheus Filipe dos Santos Reinert <62085690+shinobu-uwu@users.noreply.github.com>
2020-11-10 21:24:59 -0300

    Add key em Listbox

--- inserir_comando_view.py
+++ inserir_comando_view.py
@@ -4,7 +4,7 @@ import PySimpleGUI as sg
 class InserirComandoView:
     def __init__(self, bot):
         self.__layout = [
-            [sg.Text("Selecione um tipo de comando"), sg.Listbox([comando for comando in bot.comandos], size = (30, 3))],
+            [sg.Text("Selecione um tipo de comando"), sg.Listbox([comando for comando in bot.comandos], size = (30, 3), key = "comandos")],
             [sg.Button("Inserir", key = "inserir"), sg.Cancel()]
         ]
         self.__window = sg.Window("Inserir comando", self.__layout, element_justification = "center")
