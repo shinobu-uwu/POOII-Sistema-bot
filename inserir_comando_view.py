a026d43feccd1b175133d26a712b60d2b6f938ae
Matheus Filipe dos Santos Reinert <62085690+shinobu-uwu@users.noreply.github.com>
2020-11-10 21:26:25 -0300

    Add campo para inserir comando

--- inserir_comando_view.py
+++ inserir_comando_view.py
@@ -5,6 +5,7 @@ class InserirComandoView:
     def __init__(self, bot):
         self.__layout = [
             [sg.Text("Selecione um tipo de comando"), sg.Listbox([comando for comando in bot.comandos], size = (30, 3), key = "comandos")],
+            [sg.Text("Digite o comando"), sg.Input(key = "comando")],
             [sg.Button("Inserir", key = "inserir"), sg.Cancel()]
         ]
         self.__window = sg.Window("Inserir comando", self.__layout, element_justification = "center")
